bda1169c6501b576d41a34d986c60405ea8b7056
root <root@bitrix.test>
2026-03-01 16:31:37 +0000

    base structure, added auth/registration and personal page

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->SetTitle('Главная');
-?> 
-
-<?
+$APPLICATION->SetTitle("Главная");
+?><h1>Главная</h1>
+ <!-- <a href="authorization.php">Авторизация</a> <a href="registration.php">Регистрация</a><br> --> <br>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.form",
+	"",
+	Array(
+		"FORGOT_PASSWORD_URL" => "",
+		"PROFILE_URL" => "personal.php",
+		"REGISTER_URL" => "",
+		"SHOW_ERRORS" => "Y"
+	)
+);?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.register",
+	"",
+	Array(
+		"AUTH" => "Y",
+		"REQUIRED_FIELDS" => array("EMAIL"),
+		"SET_TITLE" => "N",
+		"SHOW_FIELDS" => array("EMAIL"),
+		"SUCCESS_PAGE" => "personal.php",
+		"USER_PROPERTY" => array(),
+		"USER_PROPERTY_NAME" => "",
+		"USE_BACKURL" => "Y"
+	)
+);?><?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
---
+++ personal.php
@@ -0,0 +1,5 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Личный кабинет");
+?><a href="index.php">Назад</a>
+<h1> Личный кабинет </h1><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
